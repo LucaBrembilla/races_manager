ffd7146cf0d36ccf51c68f469d3b8adc90a69dec
Luca Brembilla <64411437+LucaBrembilla@users.noreply.github.com>
2022-05-21 15:01:34 +0200

    Update results.dart

--- flutter application/lib/results.dart
+++ flutter application/lib/results.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:nuvole_mobili/main.dart';
 
 import './globals.dart';
 // ignore: unused_import
